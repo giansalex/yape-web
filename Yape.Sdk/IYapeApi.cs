e3f429d069c2b23f179a4b88b1698ce445d481ca
Giancarlos Salas <giansalex@gmail.com>
2020-06-21 16:56:35 -0500

    Include authorization headers

--- Yape.Sdk/IYapeApi.cs
+++ Yape.Sdk/IYapeApi.cs
@@ -16,18 +16,25 @@ namespace Yape.Sdk
         Task<Keyboard> KeyBoard();
         [Post("/api-mobile/public/identity/login/unlock")]
         Task<Identity> Login(UserLogin login);
+        [Headers("Authorization: Bearer", "Content-Type: application/x-www-form-urlencoded")]
         [Post("/customers/v2/balance")]
         Task<Balance> Balance();
+        [Headers("Authorization: Bearer")]
         [Get("/transfers/v2/history/{**filter}")]
         Task<History> History(string filter);
+        [Headers("Authorization: Bearer")]
         [Get("/api-mobile/payments")]
         Task<OrderHistory> Orders();
+        [Headers("Authorization: Bearer")]
         [Post("/api-mobile/payments")]
         Task<OrderResult> CreateOrder(Order order);
+        [Headers("Authorization: Bearer")]
         [Get("/api-mobile/payments/{orderId}")]
         Task<OrderResult> Order(int orderId);
+        [Headers("Authorization: Bearer")]
         [Delete("/api-mobile/payments/{orderId}")]
         Task<OrderResult> UndoOrder(int orderId);
+        [Headers("Authorization: Bearer")]
         [Post("/api-mobile/transfers/targetUserData")]
         Task<Customer> Customer(CustomerPhone phone);
     }
