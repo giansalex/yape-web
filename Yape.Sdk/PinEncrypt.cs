7374cc86248ea8c196295d9754652eae41d49206
Giancarlos Salas <giansalex@gmail.com>
2020-06-21 17:09:35 -0500

    Add encrypt pin

---
+++ Yape.Sdk/PinEncrypt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Yape.Sdk
+{
+    public static class PinEncrypt
+    {
+        private static Dictionary<string, string> _pines = new Dictionary<string, string>
+        {
+            {"0", "de98e1c36667"},
+            {"1", "df99e0c26766"},
+            {"2", "dc9ae3c16465"},
+            {"3", "dd9be2c06564"},
+            {"4", "da9ce5c76263"},
+            {"5", "db9de4c66362"},
+            {"6", "d89ee7c56061"},
+            {"7", "d99fe6c46160"},
+            {"8", "d690e9cb6e6f"},
+            {"9", "d791e8ca6f6e"}
+        };
+
+        public static string GetPinHash(string[] keyboard, string[] pinPassword)
+        {
+            var indexKeys = new Queue<string>();
+            foreach (var item in pinPassword)
+            {
+                var idx = Array.IndexOf(keyboard, item);
+                indexKeys.Enqueue(idx.ToString());
+            }
+
+            var init = 0;
+            var pinText = "";
+            foreach (var pin in indexKeys)
+            {
+                pinText += _pines[pin].Substring(init, 2);
+                init += 2;
+            }
+
+            return pinText;
+        }
+    }
+}
