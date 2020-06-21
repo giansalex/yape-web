e1751a5ec04804a26575390fa6e95333f48f401e
Giancarlos Salas <giansalex@gmail.com>
2020-06-20 23:22:55 -0500

    Add retrofit

--- Yape.Sdk/IYapeApi.cs
+++ Yape.Sdk/IYapeApi.cs
@@ -1,17 +1,26 @@
 ﻿using System.Threading.Tasks;
+using Refit;
 using Yape.Sdk.Entity;
 
 namespace Yape.Sdk
 {
     public interface IYapeApi
     {
+        [Get("/identity/login/start")]
         Task LoginStart();
+        [Get("/api/keyboard")]
         Task<Keyboard> KeyBoard();
+        [Post("/api-mobile/public/identity/login/unlock")]
         Task<Identity> Login(UserLogin login);
+        [Post("/customers/v2/balance")]
         Task<Balance> Balance();
+        [Get("/transfers/v2/history/0/0/7")]
         Task<History> History();
-        Task<OrderResult> Order(Order order);
+        [Get("/api-mobile/payments")]
         Task<OrderHistory> Orders();
+        [Post("/api-mobile/payments")]
+        Task<OrderResult> Order(Order order);
+        [Post("/api-mobile/transfers/targetUserData")]
         Task<Customer> Customer(CustomerPhone phone);
     }
 }
