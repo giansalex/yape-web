07f9ecd81b591c0736f9ab1d2d6242cac05f1774
Giancarlos Salas <giansalex@gmail.com>
2020-06-21 22:50:50 -0500

    Add yape client

---
+++ Yape.Sdk/IYapeClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Yape.Sdk.Entity;
+
+namespace Yape.Sdk
+{
+    public interface IYapeClient
+    {
+        Task<decimal> GetBalance();
+        
+        Task<Payment[]> ListOrders();
+
+        Task<HistoryItem[]> GetLatestHistory();
+
+        Task<Payment> CreateOrder(Order order);
+
+        Task<Payment> GetOrder(int order);
+
+        Task<bool> DeleteOrder(int order);
+    }
+}
---
+++ Yape.Sdk/YapeClient.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Yape.Sdk.Entity;
+
+namespace Yape.Sdk
+{
+    public class YapeClient : IYapeClient
+    {
+        private readonly IYapeApi _api;
+
+        public YapeClient(IYapeApi api)
+        {
+            _api = api;
+        }
+
+        public async Task<decimal> GetBalance()
+        {
+            var result = await _api.Balance();
+
+            return result.Success ? decimal.Parse(result.Response.Amount) : 0;
+        }
+
+        public async Task<Payment[]> ListOrders()
+        {
+            var result = await _api.Orders();
+
+            return result.Success ? result.Response : null;
+        }
+
+        public async Task<HistoryItem[]> GetLatestHistory()
+        {
+            var result = await _api.History("0/0/7");
+
+            return result.Success ? result.Response.Entities : null;
+        }
+
+        public async Task<Payment> CreateOrder(Order order)
+        {
+            var result = await _api.CreateOrder(order);
+
+            return result.Success ? result.Response : null;
+        }
+
+        public async Task<Payment> GetOrder(int order)
+        {
+            var result = await _api.Order(order);
+
+            return result.Success ? result.Response : null;
+        }
+
+        public async Task<bool> DeleteOrder(int order)
+        {
+            var result = await _api.UndoOrder(order);
+
+            return result.Success;
+        }
+    }
+}
