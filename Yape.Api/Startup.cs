ebbe7e85eb72a732f1423ac119c811210103b722
Giancarlos Salas <giansalex@gmail.com>
2020-06-23 08:44:38 -0500

    configure yape client service

--- Yape.Api/Startup.cs
+++ Yape.Api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Yape.Sdk;
 
 namespace Yape.Api
 {
@@ -26,6 +27,13 @@ namespace Yape.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<IPinResolver, PinEncrypt>();
+            services.AddTransient<YapeClient>();
+            services.AddTransient<IYapeClient, YapeAuthClient>(s => new YapeAuthClient(
+                s.GetService<YapeClient>(),
+                s.GetService<IYapeApi>(),
+                s.GetService<IPinResolver>()
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
