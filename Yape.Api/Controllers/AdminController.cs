b8913b6d6528c169367ec5fbc5023badaac66772
Giancarlos Salas <giansalex@gmail.com>
2020-06-23 10:59:34 -0500

    Add admin controller

---
+++ Yape.Api/Controllers/AdminController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Yape.Sdk;
+using Yape.Sdk.Entity;
+
+namespace Yape.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        private readonly IYapeClient _yape;
+
+        public AdminController(IYapeClient yape)
+        {
+            _yape = yape;
+        }
+
+        [HttpGet("History")]
+        public async Task<IEnumerable<HistoryItem>> GetHistory()
+        {
+            return await _yape.GetLatestHistory();
+        }
+
+        [HttpGet("Orders")]
+        public async Task<IEnumerable<Payment>> GetOrders()
+        {
+            return await _yape.ListOrders();
+        }
+    }
+}
