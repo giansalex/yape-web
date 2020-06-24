b8b4401678d3adffccd502af6fe0bbcc4326848a
Giancarlos Salas <giansalex@gmail.com>
2020-06-24 15:24:03 -0500

    Add order repository

---
+++ Yape.Api/Repository/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using Yape.Api.Models;
+
+namespace Yape.Api.Repository
+{
+    public interface IOrderRepository
+    {
+        Task<OrderIntent> Get(string code);
+        Task Save(string code, OrderIntent order);
+    }
+}
---
+++ Yape.Api/Services/OrderGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Yape.Api.Models;
+using Yape.Api.Repository;
+
+namespace Yape.Api.Services
+{
+    public class OrderGenerator
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderGenerator(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        public async Task<string> GenerateOrderCode()
+        {
+            OrderIntent order;
+            string code;
+            do
+            {
+                code = new Random().Next(1111, 9999).ToString();
+
+                order = await _orderRepository.Get(code);
+            } while (order != null);
+
+            return code;
+        }
+    }
+}
