544f8aa3c3ca40762a725a6dde8bfaa01afc4b88
Giancarlos Salas <giansalex@gmail.com>
2020-06-24 15:32:19 -0500

    Implement cache order repository

---
+++ Yape.Api/Repository/CacheOrderRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+using Yape.Api.Models;
+
+namespace Yape.Api.Repository
+{
+    public class CacheOrderRepository : IOrderRepository
+    {
+        private const string OrderPrefix = "yape-order-";
+        private readonly IDistributedCache _cache;
+
+        public CacheOrderRepository(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        public async Task<OrderIntent> Get(string code)
+        {
+            var json = await _cache.GetStringAsync(OrderPrefix + code);
+
+            return json == null ? null : JsonSerializer.Deserialize<OrderIntent>(json);
+        }
+
+        public async Task Save(string code, OrderIntent order)
+        {
+            if (string.IsNullOrEmpty(order.Id))
+            {
+                order.Id = Guid.NewGuid().ToString();
+                order.State = OrderState.Pending;
+            }
+
+            var json = JsonSerializer.Serialize(order);
+            await _cache.SetStringAsync(OrderPrefix + code, json);
+        }
+    }
+}
