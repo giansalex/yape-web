3bb7987a8bb749afc7ec4b8c059e7bed5c31f137
Giancarlos Salas <giansalex@gmail.com>
2020-06-29 00:18:33 -0500

    Validate state payment

--- Yape.Api/Controllers/PaymentController.cs
+++ Yape.Api/Controllers/PaymentController.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Yape.Api.Models;
+using Yape.Api.Repository;
+using Yape.Api.Services;
 using Yape.Sdk;
 using Yape.Sdk.Entity;
 
@@ -12,31 +14,54 @@ namespace Yape.Api.Controllers
     public class PaymentController : ControllerBase
     {
         private readonly IYapeClient _yape;
+        private readonly IPaymentRepository _repository;
 
-        public PaymentController(IYapeClient yape)
+        public PaymentController(IYapeClient yape, IPaymentRepository repository)
         {
             _yape = yape;
+            _repository = repository;
         }
 
-        [HttpGet("{id:int}")]
-        public async Task<IActionResult> Get(int id)
+        [HttpGet("{code}")]
+        public async Task<IActionResult> Get(string code, [FromHeader(Name = "Authorization")] string paymentId)
         {
-            var result = await _yape.GetOrder(id);
-            if (result == null)
+            var savePayment = await _repository.Get(code);
+            if (savePayment == null || savePayment.Id != paymentId)
             {
                 return NotFound();
             }
+
+            if (savePayment.State != OrderState.Pending)
+            {
+                return Ok(new
+                {
+                    Code = code,
+                    savePayment.State
+                });
+            }
+
+            var result = await _yape.GetOrder(savePayment.PaymentYapeId);
+            if (result == null)
+            {
+                return Ok(new
+                {
+                    Error = "Payment not found!"
+                });
+            }
             
+            savePayment.CompleteDate = DateTime.Now;
+            savePayment.State = result.Status;
+            await _repository.Save(code, savePayment);
+
             return Ok(new
             {
-                result.Id,
-                result.CreationTime,
-                result.Status
+                Code = code,
+                savePayment.State
             });
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] PaymentIntent intent)
+        public async Task<IActionResult> Post([FromBody] PaymentIntent intent, [FromServices] OrderGenerator generator)
         {
             if (!ModelState.IsValid)
             {
@@ -60,6 +85,10 @@ namespace Yape.Api.Controllers
             {
                 return Ok(new { Error = true });
             }
+
+            var newCode = await generator.GenerateOrderCode();
+            intent.Create = DateTime.Now;
+            await _repository.Save(newCode, intent);
 
             return Ok(new
             {
